99e6099592afb46cc3ea7026a4c47b7d9d18f232
KevinRen <kevinren@bupt.edu.cn>
2020-06-29 00:32:56 +0800

    index修改

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from '../public/img/Index.jpg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Login extends React.Component {
+    render() {
+        return (
+            <div className = "App-div" >
+            <header className = "App-header">
+            <a>123</a>
+            <a>456</a>
+            </header>
+            <footer className = "App-footer">
+            <a>ICP备案号:</a>
+            <a href='http://www.beian.miit.gov.cn'>京ICP备19033483号</a>
+            </footer>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default Login;
