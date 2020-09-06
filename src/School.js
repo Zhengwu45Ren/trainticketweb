a6dbfe614ef4177fc6559794f73c46df2da7d3e2
KevinRen <kevinren@bupt.edu.cn>
2020-09-06 22:11:11 +0800

    出入证

--- src/School.js
+++ src/School.js
@@ -3,21 +3,53 @@ import Helmet from 'react-helmet';
 import './School.css'
 
 class School extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            getTime:''
+        };
+    }
+
+    componentDidMount(){
+        fetch('http://www.chewingtogether.com:8085/school',{
+            // post提交
+            method:"GET",
+            credentials:"include",
+            headers:{
+                "Content-type":"application/json"
+            }})
+            .then(res => {
+                return res.json()
+            }).then(resdata => {
+            if(resdata.hasOwnProperty("message")) {
+                alert(resdata.message)
+            } else {
+                this.setState({getTime: resdata})
+            }
+        })
+    }
+
     render() {
         return (
             <>
             <Helmet>
                 <title>请假外出、返校权限查询通行码</title>
             </Helmet>
-            <div>
+            <div className="School-head">
+                <img className="School-div" src={require("../public/img/head.png")}/>
+            </div>
+            <div className="School-content">
                 <img className="School-div" src={require("../public/img/uppart.png")}/>
                 <img className="School-div" src={require("../public/img/midpart.png")}/>
-            </div>
             <div className="School-time">
-                <p>123</p>
+                <p>{this.state.getTime}</p>
             </div>
             <div>
-                <img className="School-div" src={require("../public/img/bottompart.png")}/>
+                    <img className="School-div" src={require("../public/img/bottompart.png")}/>
+            </div>
+            </div>
+            <div className="School-tail">
+                <img className="School-div" src={require("../public/img/tail.png")}/>
             </div>
             </>
         );
